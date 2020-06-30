afbbe1c9f949b655c5c56f63a11d5957c5bdb817
Sergei Starodubov <41199611+haxul@users.noreply.github.com>
2020-06-30 12:46:55 +0400

    Update BubbleSorting.java

--- src/main/java/simpleSorting/BubbleSorting.java
+++ src/main/java/simpleSorting/BubbleSorting.java
@@ -16,11 +16,11 @@ public class BubbleSorting {
                 .descendingIterator()
                 .forEachRemaining(list::add);
 
-        System.out.println(bubbleSorting(list, true, 0));
+        System.out.println(bubble(list, true));
     }
 
     public static List<Integer> bubbleSorting(List<Integer> list, boolean isSorted, int index) {
-        if (index >= list.size() - 1) return bubbleSorting(list, true, 0);
+        if (index == list.size() - 1) return bubbleSorting(list, true, 0);
         if (list.get(index) < list.get(index + 1)) {
             int tmp = list.get(index);
             list.set(index, list.get(index + 1));
@@ -29,5 +29,18 @@ public class BubbleSorting {
         }
         if (isSorted) return list;
         return bubbleSorting(list, true, index + 1);
+    }
+
+    public static List<Integer> bubble(List<Integer> list, boolean isSorted) {
+        for (int i = 1; i < list.size(); i ++) {
+            if (list.get(i - 1) > list.get(i)) {
+                int tmp = list.get(i);
+                list.set(i, list.get(i - 1));
+                list.set(i - 1, tmp);
+                isSorted = false;
+            }
+        }
+        if (isSorted) return list;
+        return bubble(list, true);
     }
 }
