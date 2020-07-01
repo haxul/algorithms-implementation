2347fabead50953a339729c5222572687d8b3872
haxul <s.s.starodubov@yandex.ru>
2020-07-01 18:42:30 +0400

    InfixToPostfixRun

---
+++ src/main/java/stakeAndQueque/stack/InfixToPostfixRun.java
@@ -0,0 +1,72 @@
+package stakeAndQueque.stack;
+
+import java.util.List;
+import java.util.Stack;
+
+public class InfixToPostfixRun {
+    public static void main(String[] args) {
+        String expression = infixToPostfix("(1+2)*3");
+    }
+
+    public static String infixToPostfix(String exp) {
+        // initializing empty String for result
+        String result = new String("");
+
+        // initializing empty stack
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < exp.length(); ++i) {
+            char c = exp.charAt(i);
+
+            // If the scanned character is an operand, add it to output.
+            if (Character.isLetterOrDigit(c))
+                result += c;
+
+                // If the scanned character is an '(', push it to the stack.
+            else if (c == '(')
+                stack.push(c);
+
+                //  If the scanned character is an ')', pop and output from the stack
+                // until an '(' is encountered.
+            else if (c == ')') {
+                while (!stack.isEmpty() && stack.peek() != '(')
+                    result += stack.pop();
+
+                if (!stack.isEmpty() && stack.peek() != '(') return "Invalid Expression"; // invalid expression
+                else stack.pop();
+            } else // an operator is encountered
+            {
+                while (!stack.isEmpty() && Prec(c) <= Prec(stack.peek())) {
+                    if (stack.peek() == '(')
+                        return "Invalid Expression";
+                    result += stack.pop();
+                }
+                stack.push(c);
+            }
+
+        }
+
+        // pop all the operators from the stack
+        while (!stack.isEmpty()) {
+            if (stack.peek() == '(') return "Invalid Expression";
+            result += stack.pop();
+        }
+        return result;
+    }
+
+    public static int Prec(char ch) {
+        switch (ch) {
+            case '+':
+            case '-':
+                return 1;
+
+            case '*':
+            case '/':
+                return 2;
+
+            case '^':
+                return 3;
+        }
+        return -1;
+    }
+}
