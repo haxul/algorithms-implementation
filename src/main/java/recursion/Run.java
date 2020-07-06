1812b2ce5b61467de74803b4841149d18c69b4cb
haxul <s.s.starodubov@yandex.ru>
2020-07-06 20:16:33 +0400

    recursion

--- src/main/java/recursion/Run.java
+++ src/main/java/recursion/Run.java
@@ -1,11 +1,12 @@
 package recursion;
 
+import java.util.Arrays;
+
 public class Run {
     public static void main(String[] args) {
-        int n = findTriangleNumber(7);
-        System.out.println(loopFactorial(5));
+        makeAnogram("cat");
     }
-
+    
 
     public static int findTriangleNumber(int n) {
         if (n == 1) return 1;
@@ -26,6 +27,26 @@ public class Run {
         return result;
     }
 
+    public static void makeAnogram(String word) {
+        String[] split = word.split("");
+
+        for (int i = 0; i < split.length - 1; i++) {
+            String tmp = split[i];
+            split[i] = split[i + 1];
+            split[i + 1 ] = tmp;
+            String newString = Arrays.toString(split);
+            rotate(newString);
+        }
+    }
+    static void rotate(String word) {
+        String[] split = word.split("");
+        for (int i = split.length - 1; i > 0; i--) {
+            String tmp = split[i];
+            split[i] = split[i -1];
+            split[i - 1 ] = tmp;
+            System.out.println();
+        }
+    }
 }
 
 
