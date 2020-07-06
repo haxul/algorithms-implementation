2487777e8ed937f6c7776b46d3095cbacf76c0b6
haxul <s.s.starodubov@yandex.ru>
2020-07-06 21:23:20 +0400

    fix MergeSorting

--- src/main/java/recursion/MergeSorting.java
+++ src/main/java/recursion/MergeSorting.java
@@ -18,35 +18,37 @@ public class MergeSorting {
         right.add(212);
         right.add(22222);
         List<Integer> merge = new ArrayList<>();
-        mergeSorting(left, right, merge, right.size() - 1, left.size() - 1 );
-        System.out.println(merge);
+        mergeSorting(left, right, merge, right.size() - 1, left.size() - 1);
+
     }
 
+//    public static List<Integer> mergeSort(List<Integer> list) {
+//        if (list.size() == 1) return list;
+//
+//
+//    }
 
     public static List<Integer> mergeSorting(List<Integer> left, List<Integer> right, List<Integer> merge, int rightLast, int leftLast) {
         if (left.isEmpty() && right.isEmpty()) return merge;
-        else {
-            if (left.isEmpty()) {
-                merge.add(right.get(rightLast));
-                right.remove(rightLast);
-                return mergeSorting(left, right, merge, rightLast - 1, 0);
-            }
-            else if (right.isEmpty()) {
-                merge.add(left.get(left.size() - 1));
-                left.remove(left.size() - 1);
-                return mergeSorting(left, right, merge, 0, leftLast - 1);
-            }
-             else if (left.get(leftLast) > right.get(rightLast)) {
-                merge.add(left.get(leftLast));
-                left.remove(leftLast);
-                return mergeSorting(left, right, merge, rightLast, leftLast - 1);
-            } else  {
-                merge.add(right.get(rightLast));
-                right.remove(rightLast);
-                return mergeSorting(left, right, merge, rightLast - 1 ,leftLast);
-            }
-
+        else if (left.isEmpty()) {
+            merge.add(right.get(rightLast));
+            right.remove(rightLast);
+            return mergeSorting(left, right, merge, rightLast - 1, 0);
+        } else if (right.isEmpty()) {
+            merge.add(left.get(left.size() - 1));
+            left.remove(left.size() - 1);
+            return mergeSorting(left, right, merge, 0, leftLast - 1);
+        } else if (left.get(leftLast) > right.get(rightLast)) {
+            merge.add(left.get(leftLast));
+            left.remove(leftLast);
+            return mergeSorting(left, right, merge, rightLast, leftLast - 1);
+        } else {
+            merge.add(right.get(rightLast));
+            right.remove(rightLast);
+            return mergeSorting(left, right, merge, rightLast - 1, leftLast);
         }
+
+
     }
 
 }
