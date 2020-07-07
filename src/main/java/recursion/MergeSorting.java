c22df1a79b52b7c84530705c163f58620eae5619
haxul <s.s.starodubov@yandex.ru>
2020-07-07 19:56:46 +0400

    fix MergeSorting

--- src/main/java/recursion/MergeSorting.java
+++ src/main/java/recursion/MergeSorting.java
@@ -1,10 +1,7 @@
 package recursion;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collector;
-import java.util.stream.Collectors;
 
 public class MergeSorting {
     public static void main(String[] args) {
