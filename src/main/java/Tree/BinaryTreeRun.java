886c5704d464632facceb018cd49054ad8ec16e6
haxul <s.s.starodubov@yandex.ru>
2020-07-13 22:05:52 +0400

    find successor

--- src/main/java/Tree/BinaryTreeRun.java
+++ src/main/java/Tree/BinaryTreeRun.java
@@ -67,6 +67,22 @@ class BinaryTree {
         }
     }
 
+    private Node getSuccessor(Node delNode) {
+        Node successorParent = delNode;
+        Node successor = delNode;
+        Node current = delNode.rightChild;
+        while (current != null) {
+            successorParent = successor;
+            successor = current;
+            current = current.leftChild;
+        }
+        if (successor != delNode.rightChild) {
+            successorParent.leftChild = successor.rightChild;
+            successor.rightChild = delNode.rightChild;
+        }
+        return successor;
+    }
+
     public Node find(int key) {
         return internalFind(key, root);
     }
