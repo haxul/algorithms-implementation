30e8a66bbfb59b5a550b4af6208e3c9ef659b08e
haxul <s.s.starodubov@yandex.ru>
2020-07-14 21:31:41 +0400

    red black tree

--- src/main/java/Tree/RedBlackTreeRun.java
+++ src/main/java/Tree/RedBlackTreeRun.java
@@ -181,8 +181,7 @@ class RedBlackTree {
     private RedBlackNode header;
     private static RedBlackNode nullNode;
 
-    static         // Static initializer for nullNode
-    {
+    static {
         nullNode = new RedBlackNode(null);
         nullNode.left = nullNode.right = nullNode;
     }
@@ -196,26 +195,6 @@ class RedBlackTree {
     private static RedBlackNode grand;
     private static RedBlackNode great;
 
-
-    // Test program
-    public static void main(String[] args) {
-        RedBlackTree t = new RedBlackTree();
-        final int NUMS = 400000;
-        final int GAP = 35461;
-
-        System.out.println("Checking... (no more output means success)");
-
-        for (int i = GAP; i != 0; i = (i + GAP) % NUMS)
-            t.insert(new Integer(i));
-
-        if (((Integer) (t.findMin())).intValue() != 1 ||
-                ((Integer) (t.findMax())).intValue() != NUMS - 1)
-            System.out.println("FindMin or FindMax error!");
-
-        for (int i = 1; i < NUMS; i++)
-            if (((Integer) (t.find(new Integer(i)))).intValue() != i)
-                System.out.println("Find error1!");
-    }
 }
 
 
