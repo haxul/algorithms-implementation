f652e3c0582013e6dd3f3db98026a774bb1d0d55
haxul <s.s.starodubov@yandex.ru>
2020-07-15 21:43:16 +0400

    234 tree

--- src/main/java/Tree/Tree234Run.java
+++ src/main/java/Tree/Tree234Run.java
@@ -1,15 +1,24 @@
 package Tree;
 
+import lombok.Data;
+
 public class Tree234Run {
     public static void main(String[] args) {
-
+        DataItem item = new DataItem(77);
     }
 }
 
+@Data
+class DataItem {
+    public long dData;
 
-class Tree234 {
-    public String head;
+    public DataItem(long dd) {
+        dData = dd;
+    }
+
+    public void displayItem() {
+        System.out.print("/" + dData);
+    }
 }
-
 
 
