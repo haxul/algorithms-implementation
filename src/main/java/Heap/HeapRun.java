980351707e91e585972b72070af74ad595dad3cf
Sergei Starodubov <41199611+haxul@users.noreply.github.com>
2020-07-17 14:35:48 +0400

    Update HeapRun.java

--- src/main/java/Heap/HeapRun.java
+++ src/main/java/Heap/HeapRun.java
@@ -12,30 +12,64 @@ public class HeapRun {
 @Data
 @AllArgsConstructor
 class Node {
-    private int id;
+    public Node(int key) {
+        this.key = key;
+    }
+
+    private int key;
     private String value;
 }
 
 
 class Heap {
-    private Node heapArray[];
+    private Node heapArray[] = new Node[30];
+    int currentSize = 0;
+    int maxSize = heapArray.length;
 
-    public void insert(Node nd) {
+    public boolean insert(int key) {
+        if (currentSize == maxSize)
+            return false;
+        Node newNode = new Node(key);
+        heapArray[currentSize] = newNode;
+        trickleUp(currentSize++);
+        return true;
+    }
+
+    private void trickleUp(int index) {
+        int parent = (index - 1) / 2;
+        Node bottom = heapArray[index];
+        while (index > 0 &&
+                heapArray[parent].getKey() < bottom.getKey()) {
+            heapArray[index] = heapArray[parent];
+            index = parent;
+            parent = (parent - 1) / 2; 
+        }
+        heapArray[index] = bottom;
     }
 
     public Node remove() {
-        return null;
-    }
-}
-
-class priorityQueue {
-    private Heap theHeap;
-
-    public void insert(Node nd) {
-        theHeap.insert(nd);
+        Node root = heapArray[0];
+        heapArray[0] = heapArray[--currentSize];
+        trickleDown(0);
+        return root;
     }
 
-    public Node remove() {
-        return theHeap.remove();
+    private void trickleDown(int index) {
+        int largerChild;
+        Node top = heapArray[index];
+        while (index < currentSize / 2) {
+            int leftChild = 2 * index + 1;
+            int rightChild = leftChild + 1;
+            if (rightChild < currentSize && heapArray[leftChild].getKey() <
+                    heapArray[rightChild].getKey())
+                largerChild = rightChild;
+            else largerChild = leftChild;
+            if (top.getKey() >= heapArray[largerChild].getKey())
+                break;
+            heapArray[index] = heapArray[largerChild];
+            index = largerChild;
+
+        }
+        heapArray[index] = top;
     }
 }
