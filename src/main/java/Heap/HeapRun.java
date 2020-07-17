9750e6c48586780bffbbcb8967c0a27df12464f7
Sergei Starodubov <41199611+haxul@users.noreply.github.com>
2020-07-17 13:00:57 +0400

    Update HeapRun.java

--- src/main/java/Heap/HeapRun.java
+++ src/main/java/Heap/HeapRun.java
@@ -1,7 +1,41 @@
 package Heap;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
 public class HeapRun {
     public static void main(String[] args) {
-        
+
+    }
+}
+
+@Data
+@AllArgsConstructor
+class Node {
+    private int id;
+    private String value;
+}
+
+
+class Heap {
+    private Node heapArray[];
+
+    public void insert(Node nd) {
+    }
+
+    public Node remove() {
+        return null;
+    }
+}
+
+class priorityQueue {
+    private Heap theHeap;
+
+    public void insert(Node nd) {
+        theHeap.insert(nd);
+    }
+
+    public Node remove() {
+        return theHeap.remove();
     }
 }
